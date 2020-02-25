1c67b78de659d42c6b5164107a640b263bd5a20b
tongnamuu <33937684+thban33@users.noreply.github.com>
2020-02-26 01:32:59 +0900

    Giving props to components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food({ name }) {
+  return <h2>I like {name}!!!</h2>;
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
-      <Potato />
+      <Food name="chicken" />
+      <Food name="Kimchi" />
+      <Food name="Hot Noodle" />
     </div>
   );
 }
