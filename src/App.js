7b4b0bc9b3bc0e2df3f8e1b31a292fccf1a2896e
tongnamuu <33937684+thban33@users.noreply.github.com>
2020-02-26 01:57:49 +0900

    Render a FoodList with map

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import React from "react";
 
-function Food({ name }) {
-  return <h2>I like {name}!!!</h2>;
+function Food(props) {
+  return (
+    <div>
+      <h2>I like {props.name}!!!</h2>
+      <img src={props.pic}></img>
+    </div>
+  );
 }
-
+let FoodList = [
+  {
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+  },
+  {
+    name: "Chicken",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2019/06/Coke_Chicken_18-.jpg"
+  },
+  {
+    name: "Hot Noodle",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2019/05/Soy-Bean-Sprout-Chewy-Noodles_01-.jpg"
+  },
+  {
+    name: "BibimBob",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+  }
+];
 function App() {
   return (
     <div className="App">
       <h1>Hello React</h1>
-      <Food name="chicken" />
-      <Food name="Kimchi" />
-      <Food name="Hot Noodle" />
+      {FoodList.map(dish => (
+        <Food name={dish.name} pic={dish.image} />
+      ))}
     </div>
   );
 }
