164a2f9e67031c12702a8f3894ad19b30c5a2f18
tongnamuu <33937684+thban33@users.noreply.github.com>
2020-02-26 19:08:56 +0900

    HTML Completed

--- src/App.js
+++ src/App.js
@@ -1,102 +1,51 @@
 import React from "react";
-
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 class App extends React.Component {
-  constructor(props) {
-    console.log("Constructor");
-    super(props);
-  }
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   };
-  add = () => {
-    console.log("add");
-    this.setState(current => ({
-      count: current.count + 1
-    }));
-  };
-  minus = () => {
-    console.log("minus");
-    this.setState(abc => ({
-      count: abc.count - 1
-    }));
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies: movies, isLoading: false });
   };
   componentDidMount() {
-    console.log("ComponentDidMount after render");
-  }
-  componentDidUpdate() {
-    console.log("component just updated");
-  }
-  componentWillUnmount() {
-    console.log("Destroying Component");
+    this.getMovies();
   }
   render() {
-    console.log("rendering");
+    const { isLoading, movies } = this.state;
     return (
-      <div>
-        <h1>This is Class Component Counter : {this.state.count}</h1>
-        <button onClick={this.add}>add</button>
-        <button onClick={this.minus}>minus</button>
-      </div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
-
-// const FoodList = [
-//   {
-//     id: 1,
-//     name: "Kimchi",
-//     image:
-//       "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-//     rating: 5
-//   },
-//   {
-//     id: 2,
-//     name: "Chicken",
-//     image:
-//       "http://aeriskitchen.com/wp-content/uploads/2019/06/Coke_Chicken_18-.jpg",
-//     rating: 3
-//   },
-//   {
-//     id: 3,
-//     name: "Hot Noodle",
-//     image:
-//       "http://aeriskitchen.com/wp-content/uploads/2019/05/Soy-Bean-Sprout-Chewy-Noodles_01-.jpg",
-//     rating: 5
-//   },
-//   {
-//     id: 4,
-//     name: "BibimBob",
-//     image:
-//       "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-//     rating: 4
-//   }
-// ];
-
-// function Food({ name, pic, rating }) {
-//   return (
-//     <div>
-//       <h2>I like {name}!!!</h2>
-//       <h4>{rating}/5.0</h4>
-//       <img src={pic} alt={name}></img>
-//     </div>
-//   );
-// }
-// Food.propTypes = {
-//   name: PropTypes.string.isRequired,
-//   pic: PropTypes.string.isRequired,
-//   rating: PropTypes.number.isRequired
-// };
-
-// function renderFood(dish) {
-//   //console.log(dish);
-//   return (
-//     <Food
-//       key={dish.id}
-//       name={dish.name}
-//       pic={dish.image}
-//       rating={dish.rating}
-//     />
-//   );
-// }
 
 export default App;
